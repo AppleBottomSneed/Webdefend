b8200a65aefe6a535bf93e2db2458f8dc6c3e76b
AppleBottomSneed <20092387@tafe.wa.edu.au>
2024-04-05 16:31:29 +0800

    Fixed importing

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+/**Import images here */
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="kat__header section__padding" id="home">
+      <div className="kat__header-content">
+        <h1>
+          <span className="kat__text-blue">Tax</span>
+          <span className="kat__text-black"> done</span>
+          <span className="kat__text-red"> right</span>
+          <span className="kat__text-black">.</span>
+        </h1>
+        <p>
+        Here at KAT Accounting Services we ensure our clients can sleep easy knowing their business is financially safe and sound.</p>
+        <p>We combine our accounting and bookkeeping in a neat and affordable package hassle-free. </p>
+        <p>Call us for a free consultation and we’ll get you sorted.</p>
+        <div className="kat__header-content__input">
+          <button type="button">Contact Us</button>
+        </div>
+      </div>
+      <div className="kat__header-image">
+        
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+/**Images import */
+import "./navbar.css";
+
+/* Creates the <Menu /> shortcut */
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#services">Services</a>
+    </p>
+    <p>
+      <a href="#resources">Resources</a>
+    </p>
+    <p>
+      <a href="#whyus">About Us</a>
+    </p>
+    <p>
+      <a href="#contact">Contact</a>
+    </p>
+  </>
+);
+
+//BEM = block element modifier
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="kat__navbar">
+      <div className="kat__navbar-links">
+        <div className="kat__navbar-links_logo">
+          <div className="kat__navbar-links_logo_big">
+            {/* <img src={images.logoPdf} alt="logo" /> */}
+          </div>
+          <div className="kat__navbar-links_logo_mobile">
+            {/* <img src={images.logoSmall} alt="logo" /> */}
+          </div>
+        </div>
+        <div className="kat__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="kat__navbar-sign">
+        <p>(08) 64603830</p>
+      </div>
+      <div className="kat__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine size={27} onClick={() => setToggleMenu(false)}/>
+        ) : (
+          <RiMenu3Line size={27} onClick={() => setToggleMenu(true)}/>
+        )}
+        {toggleMenu && (
+          <div className="kat__navbar-menu_container scale-up-center">
+            <div className="kat__navbar-menu_container-links">
+              <Menu />
+            </div>
+            <div className="kat__navbar-menu_container-links-sign">
+              <p>(08) 64603830</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
