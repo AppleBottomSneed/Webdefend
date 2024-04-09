ffc11a7f09cccffee245273fe2c8abe8f967e1c7
Placardo <20092387@tafe.wa.edu.au>
2024-04-09 13:57:40 +0800

    parallax added

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,50 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./services.css";
 import { data } from "../../constants";
  
 
-const ServiceCard = ({service: { imgUrl, title, description }}) => (
-  <div className="webdefend-infocard webdefend-container-padding">
-    <div className="webdefend-infocard-feature">
-      <div className="webdefend-infocard-icon">
-        <img src={imgUrl} alt="business icon" />
-      </div>
-      <div className="webdefend-infocard-title">
-        <p>{title}</p>
-      </div>
-    </div>
-    <div className="webdefend-infocard-description">
-        <p>{description}</p>
-    </div>
-    {/*
-    <div className="webdefend-infocard-button">
-      <button>Learn More</button>
-    </div>
-    */} 
-  </div>
-);
+function Services() {
+  const [offset, setOffset] = useState({ offsetX: 0, offsetY: 0 });
 
-
-const Services = () => {
-    
+  const handleMouseMove = (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    const offsetX = -(mouseX / window.innerWidth - 0.5) * 30; // Adjust sensitivity here
+    const offsetY = -(mouseY / window.innerHeight - 0.5) * 30; // Adjust sensitivity here
+    setOffset({ offsetX, offsetY });
+  };
 
   return (
-    <div className="section__padding " id="services">
-      <div className="webdefend-container webdefend-container-padding">
-        <div className="webdefend-title">
-          <p>Our Services</p>
+    <div className="parallax-container" onMouseMove={handleMouseMove}>
+      <div className="parallax-image-target" style={{ transform: `translate(${offset.offsetX}px, ${offset.offsetY}px)` }}>  
+        <div className="parallax-image-border" style={{ transform: `translate(${offset.offsetX}px, ${offset.offsetY}px)` }}>
+          <div className="parallax-image-aim" style={{ transform: `translate(${offset.offsetX}px, ${offset.offsetY}px)` }}>
+            
+          </div>
         </div>
-        <div className="webdefend-infocard-flex">
-          {/* {data.services.map((service) => <ServiceCard service={service}/>)} */} 
-        </div>
-        {/*
-        <div className="webdefend-infocard-viewall">
-          <button>View All Services</button>
-        </div>
-        */} 
       </div>
+      
     </div>
+    
   );
-};
+}
 
 export default Services;
